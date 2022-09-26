5bffc560dbdeed3e574714c4c9da51f44d6eb75e
Alwin Arrasyid <alwin.ridd@gmail.com>
2022-09-26 19:19:31 +0700

    handle change and select all

--- src/MyAutocomplete.tsx
+++ src/MyAutocomplete.tsx
@@ -1,6 +1,8 @@
 import { Autocomplete, Checkbox, TextField } from "@mui/material";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { SyntheticEvent, useCallback, useState } from "react";
+import { SettingsSuggestOutlined } from "@mui/icons-material";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -17,13 +19,42 @@ type MyAutocompleteProps = {
 
 function MyAutocomplete(props: MyAutocompleteProps) {
   const { options, label } = props;
+  const [selectedItems, setSelectedItems] = useState<MyAutocompleteOption[]>(
+    []
+  );
+  const isAllSelected = selectedItems.length === options.length;
+  const handleChange = useCallback(
+    (
+      event: SyntheticEvent,
+      value: MyAutocompleteOption | Array<MyAutocompleteOption>,
+      reason: string
+    ) => {
+      if (Array.isArray(value)) {
+        const selectAll = value.find(
+          (selected) => selected.value === "select-all"
+        );
+        const isSelectAllSelected = selectAll !== undefined;
+
+        if (isSelectAllSelected) {
+          setSelectedItems([...options]);
+        } else {
+          setSelectedItems(value);
+        }
+      } else {
+        setSelectedItems([value]);
+      }
+    },
+    []
+  );
 
   return (
     <Autocomplete
       id="my-autocomplete"
-      options={options}
+      options={[{ value: "select-all", label: "Select All" }, ...options]}
       getOptionLabel={(option) => option.label}
       renderOption={(props, option, { selected }) => {
+        const selectAllProps =
+          option.value === "select-all" ? { checked: isAllSelected } : {};
         return (
           <li {...props}>
             <Checkbox
@@ -31,6 +62,7 @@ function MyAutocomplete(props: MyAutocompleteProps) {
               checkedIcon={checkedIcon}
               style={{ marginRight: 8 }}
               checked={selected}
+              {...selectAllProps}
             />
             {option.label}
           </li>
@@ -39,6 +71,8 @@ function MyAutocomplete(props: MyAutocompleteProps) {
       renderInput={(params) => {
         return <TextField {...params} label={label} variant="standard" />;
       }}
+      value={selectedItems}
+      onChange={handleChange}
       disableCloseOnSelect
       multiple
     />
